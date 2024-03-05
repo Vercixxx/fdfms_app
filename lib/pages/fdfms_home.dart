9d2edfd721bb23cda0883a743aa0a499362c1f36
Vercixxx <krz.sluzalek@gmail.com>
2024-03-05 16:38:59 +0100

    Add dynamic post fetching to FdfmsHome.

--- lib/pages/fdfms_home.dart
+++ lib/pages/fdfms_home.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
 
-class FdfmsHome extends StatelessWidget {
+// Scripts
+import '../scripts/get_posts.dart';
+
+class FdfmsHome extends StatefulWidget {
   const FdfmsHome({Key? key}) : super(key: key);
+
+  @override
+  _FdfmsHomeState createState() => _FdfmsHomeState();
+}
+
+class _FdfmsHomeState extends State<FdfmsHome> {
+  late Future<List<dynamic>> postsFuture = fetchPosts();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text('FDFMS Home'),
+      body: FutureBuilder<List<dynamic>>(
+        future: postsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return Column(
+              children: [
+                const Text(
+                  'Recent Posts:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      var post = snapshot.data?[index];
+                      return ListTile(
+                        title: Text(post['title']),
+                        subtitle: Text(
+                            'By ${post['author_username']} on ${post['posted_date']}'),
+                        trailing: Text(post['content']),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
---
+++ lib/scripts/get_posts.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:get_storage/get_storage.dart';
+
+Future<List<dynamic>> fetchPosts() async {
+  final storage = GetStorage();
+  String jwt = storage.read('jwt');
+  Map<String, dynamic> jwtToken = jsonDecode(jwt);
+  String? accessToken = jwtToken['access'];
+
+  try {
+    final response = await http.get(
+      Uri.parse('http://127.0.0.1:8000/api/app/posts/get/drivers/'),
+      headers: {
+        'Authorization': 'JWT $accessToken',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final result = jsonDecode(response.body);
+      return result;
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  } catch (error) {
+    throw Exception('Failed to load posts');
+  }
+}
